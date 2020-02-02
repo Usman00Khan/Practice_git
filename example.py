d866b752e6c9c2de46d3c932f31725fc79e3c58f
usman00khan <uk80718@gmail.com>
2020-02-02 14:32:47 +0530

    First commit

---
+++ example.py
@@ -0,0 +1,15 @@
+class Sample:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def sum_int(self):
+        return self.x + self.y
+
+
+if __name__ == "__main__":
+    s = Sample(1, 2)
+    # print("\U+1F644")
+    print("\U0001f600")
+    print("\N{face with rolling eyes}") 
+

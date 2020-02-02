ef12f3de85d98674b6f04db5ef7a8daef2cc47d3
usman00khan <uk80718@gmail.com>
2020-02-02 16:56:01 +0530

    formatted

--- example.py
+++ example.py
@@ -8,12 +8,13 @@ class Sample:
 
     def sub_int(self):
         return self.x-self.y
-    
+
     def mul_int(self):
         return self.x*self.y
 
     def div_int(self):
         return self.x/self.y
+
 
 if __name__ == "__main__":
     s = Sample(1, 2)
@@ -25,5 +26,4 @@ if __name__ == "__main__":
     s.div_int()
     # print("\U+1F644")
     print("\U0001f600")
-    print("\N{face with rolling eyes}") 
-
+    print("\N{face with rolling eyes}")
